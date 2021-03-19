9829d2c5db9c5fd98e4a23cfc16c8941401b8ab1
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2021-03-19 12:48:28 -0300

    Iniciando conceitos de POO

---
+++ POO/OO.js
@@ -0,0 +1,19 @@
+//Not executable code!!!
+
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+
+//OO
+objeto ={
+    valor1,
+    valor2,
+    valor3,
+    processamento()
+}
+
+objeto.processamento() //Foco passou a ser o objeto
+
+
+//Princípios importantes
